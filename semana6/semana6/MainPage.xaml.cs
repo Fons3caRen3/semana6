b9a31aa9b5929eb47c93b2531ae204c18cc8bda9
Fausto Almeida <faustecn@hotmail.com>
2023-01-17 19:48:14 -0500

    Agregar archivos de proyecto.

---
+++ semana6/semana6/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace semana6
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private const string Url = "http://192.168.167.178/moviles/post.php";
+        private readonly HttpClient client= new HttpClient();
+        private ObservableCollection<semana6.Datos> _post;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<semana6.Datos> posts = JsonConvert.DeserializeObject<List<semana6.Datos>>(content);
+            _post = new ObservableCollection<semana6.Datos>(posts);
+             
+            MyListView.ItemsSource = _post;
+        }
+
+        private async void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new registro());
+        }
+    }
+}
---
+++ semana6/semana6/registro.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            try {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtId.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+
+                cliente.UploadValues("http://192.168.167.178/moviles/post.php", "POST", parametros);
+            }
+            catch (Exception ex) {
+
+                DisplayAlert("ALERTA", ex.Message, "CERRAR");
+            }
+            
+        }
+
+        private async void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
